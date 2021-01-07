3b4cb1cbdd7cb4ddcbdbca45ad058ba5af281c00
xJuhix <49311688+xJuhix@users.noreply.github.com>
2021-01-07 20:37:22 +0100

    added logo to navbar, made general updates, fixing errors

---
+++ src/components/home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Heading from "../layout/Heading";
+
+
+export function Home() {
+	return (
+		<>
+			<Heading title="RAWG Viedo Games" />
+		</>
+	);
+}
+
+export default Home;
--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,8 +1,6 @@
 import React from "react";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
+import {Nav, Navbar, Container, Form, FormControl, Button } from "react-bootstrap";
 import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
-import Container from "react-bootstrap/Container";
 import Home from "../home/Home";
 import Contact from "../contact/Contact";
 import ArticleDetail from "../article/ArticleDetail";
@@ -12,7 +10,15 @@ function Layout() {
 		<Router>
 			<Navbar bg="dark" variant="dark" expand="lg">
 				<NavLink to="/" exact>
-					<Navbar.Brand>RAWG Video Games</Navbar.Brand>
+					<Navbar.Brand href="#home">
+						<img
+							src="../images/logo.png"
+							width="30"
+							height="30"
+							className="d-inline-block align-top"
+							alt="WittigNotes logo"
+      					/>
+					</Navbar.Brand>
 				</NavLink>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
@@ -20,15 +26,31 @@ function Layout() {
 						<NavLink to="/" exact className="nav-link">
 							Home
 						</NavLink>
+						<NavLink to="/books" className="nav-link">
+							Books
+						</NavLink>
+						<NavLink to="/economics" className="nav-link">
+							Contact
+						</NavLink>
+						<NavLink to="/productivity" className="nav-link">
+							Contact
+						</NavLink>
 						<NavLink to="/contact" className="nav-link">
 							Contact
 						</NavLink>
 					</Nav>
+					<Form inline>
+					<FormControl type="text" placeholder="Search" className="mr-sm-2" />
+					<Button variant="outline-light">Search</Button>
+				</Form>
 				</Navbar.Collapse>
 			</Navbar>
 			<Container>
 				<Switch>
 					<Route path={["/", "/home"]} exact component={Home} />
+					<Route path="/books" component={Contact} />
+					<Route path="/economics" component={Contact} />
+					<Route path="/productivity" component={Contact} />
 					<Route path="/contact" component={Contact} />
 					<Route path="/article/:id" component={ArticleDetail} />
 				</Switch>
