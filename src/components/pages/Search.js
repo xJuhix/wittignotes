48f7c8cc7e4d91cfc6ac63f5e86d1a2108c2c04d
xJuhix <49311688+xJuhix@users.noreply.github.com>
2021-01-12 10:28:57 +0100

    Updated Headings and pages folder

--- src/components/pages/Search.js
+++ src/components/pages/Search.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Heading from "../layout/Heading";
 import ArticleList from "../article/ArticleList";
 
 export function Search() {
 	return (
 		<>
+            <Heading title="Search Articles"/>
 			<ArticleList />
 		</>
 	);
