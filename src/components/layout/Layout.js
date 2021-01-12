066082ed087f1bc53147590ab856a1aaadf2a229
xJuhix <49311688+xJuhix@users.noreply.github.com>
2021-01-12 21:19:57 +0100

    Cleaned up folder structure, styled header

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,23 +1,25 @@
 import React from "react";
 import { Nav, Navbar, Container } from "react-bootstrap";
 import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
-import Home from "../home/Home";
+import Home from "../pages/Home";
+import Books from "../pages/Books";
+import Economics from "../pages/Economics";
+import Productivity from "../pages/Productivity";
 import Contact from "../pages/Contact";
 import ArticleList from "../article/ArticleList";
 import ArticleDetail from "../article/ArticleDetail";
-
-
+import logo from "../images/logo.png";
 
 function Layout() {
 	return (
 		<Router>
-			<Navbar bg="dark" variant="dark" expand="lg">
+			<Navbar expand="lg">
 				<NavLink to="/" exact>
 					<Navbar.Brand href="#home">
 						<img
-							src="../images/logo.png"
-							width="30"
-							height="30"
+							src={logo}
+							width="250"
+							height="70"
 							className="d-inline-block align-top"
 							alt="WittigNotes logo"
       					/>
@@ -25,10 +27,7 @@ function Layout() {
 				</NavLink>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
-					<Nav className="mr-auto">
-						<NavLink to="/" exact className="nav-link">
-							Home
-						</NavLink>
+					<Nav className="ml-auto">
 						<NavLink to="/books" className="nav-link">
 							Books
 						</NavLink>
@@ -50,9 +49,9 @@ function Layout() {
 			<Container>
 				<Switch>
 					<Route path={["/", "/home"]} exact component={Home} />
-					<Route path="/books" component={Contact} />
-					<Route path="/economics" component={Contact} />
-					<Route path="/productivity" component={Contact} />
+					<Route path="/books" component={Books} />
+					<Route path="/economics" component={Economics} />
+					<Route path="/productivity" component={Productivity} />
 					<Route path="/contact" component={Contact} />
 					<Route path="/article" component={ArticleList} />
 					<Route path="/article/:id" component={ArticleDetail} />
---
+++ src/components/pages/Books.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "../layout/Heading";
+import ContactForm from "../contact/ContactForm";
+
+export function Contact() {
+	return (
+		<>
+            <Heading title="Contact" />
+			<ContactForm />
+		</>
+	);
+}
+
+export default Contact;
