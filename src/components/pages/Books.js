85145ce288c58d2a5c1aed2cdb6fc7036cd81cd5
xJuhix <49311688+xJuhix@users.noreply.github.com>
2021-01-14 17:47:57 +0100

    Making Updates

--- src/components/pages/Books.js
+++ src/components/pages/Books.js
@@ -1,14 +1,31 @@
 import React from "react";
 import Heading from "../layout/Heading";
 import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 
 
 export function Contact() {
 	return (
 		<>
-		<Jumbotron className="jumbotron-Books" style={{ backgroundImage: "url(images/booksbanner.jpg)" }}>
-        </Jumbotron>
-        <Heading title="Books" />
+		<Container>
+			<Jumbotron className="jumbotron-Books" style={{ backgroundImage: "url(images/booksbanner.jpg)" }}>
+        	</Jumbotron>
+        	<Heading title="Books" />
+			<p>
+				Nam tincidunt, sapien nec congue porta, tellus risus ullamcorper mi, a rutrum justo eros pretium libero. Nullam vel enim id mauris eleifend finibus et ac orci. Aliquam metus massa,
+                aliquam quis arcu sit amet, consectetur faucibus urna. Suspendisse massa diam, efficitur eu massa euismod, pretium lacinia magna. Donec mi orci, sollicitudin in luctus a, varius
+                eget massa.
+			</p>
+		</Container>
+		<Container className="container-articles">
+			<Row>
+				<h2>Book Articles</h2>
+			</Row>
+
+		</Container>
 		</>
 	);
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,6 +2,9 @@ import React from "react";
 import Heading from "../layout/Heading";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Container from "react-bootstrap/Container";
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
 
 
 export function Home() {
@@ -12,6 +15,26 @@ export function Home() {
 				<Heading title="My opinions and views on Books, Economics and Productivity" />
 			</Container>	
 		</Jumbotron>
+
+		<h2>Meet Me</h2>
+		<Container>		
+		<Row>
+			<Col className="intro-Image">
+				<img src="images/fw.jpeg" class="img-responsive header-img" alt="Felix Wittig"/>
+			</Col>
+			<Col className="intro-Text">
+				<p>
+					Nam tincidunt, sapien nec congue porta, tellus risus ullamcorper mi, a rutrum justo eros pretium libero. Nullam vel enim id mauris eleifend finibus et ac orci. Aliquam metus massa,
+					aliquam quis arcu sit amet, consectetur faucibus urna. Suspendisse massa diam, efficitur eu massa euismod, pretium lacinia magna. Donec mi orci, sollicitudin in luctus a, varius
+					eget massa.
+				</p>
+			</Col>
+		</Row>
+		</Container>
+
+		<Container className="recent-Articles">
+			
+		</Container>
 		</>
 	);
 }
