2f47db00d362c2cc8bcc1f8f2d391431df75251f
xJuhix <49311688+xJuhix@users.noreply.github.com>
2021-01-14 19:07:23 +0100

    Updating Homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,6 +4,7 @@ import Jumbotron from "react-bootstrap/Jumbotron";
 import Container from "react-bootstrap/Container";
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Felix from "../images/fw.png";
 
 
 
@@ -17,12 +18,14 @@ export function Home() {
 		</Jumbotron>
 
 		<h2>Meet Me</h2>
-		<Container>		
+
+		<Container className="about-Text">		
 		<Row>
-			<Col className="intro-Image">
-				<img src="images/fw.jpeg" class="img-responsive header-img" alt="Felix Wittig"/>
+			<Col sm={5} className="intro-Image">
+				<img src={Felix} alt="Felix Wittig" width="400"
+				height="300"/>
 			</Col>
-			<Col className="intro-Text">
+			<Col sm={7} className="intro-Text">
 				<p>
 					Nam tincidunt, sapien nec congue porta, tellus risus ullamcorper mi, a rutrum justo eros pretium libero. Nullam vel enim id mauris eleifend finibus et ac orci. Aliquam metus massa,
 					aliquam quis arcu sit amet, consectetur faucibus urna. Suspendisse massa diam, efficitur eu massa euismod, pretium lacinia magna. Donec mi orci, sollicitudin in luctus a, varius
