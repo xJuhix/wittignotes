f082b2ededa1ba580891afe402974618d6b63ff2
xJuhix <49311688+xJuhix@users.noreply.github.com>
2021-01-15 15:05:04 +0100

    Updating styles

--- src/components/pages/Books.js
+++ src/components/pages/Books.js
@@ -14,11 +14,13 @@ export function Books() {
         	</Jumbotron>
 		<div className="books-Header">
         	<Heading title="Books" />
-			<p>
-				Nam tincidunt, sapien nec congue porta, tellus risus ullamcorper mi, a rutrum justo eros pretium libero. Nullam vel enim id mauris eleifend finibus et ac orci. Aliquam metus massa,
-                aliquam quis arcu sit amet, consectetur faucibus urna. Suspendisse massa diam, efficitur eu massa euismod, pretium lacinia magna. Donec mi orci, sollicitudin in luctus a, varius
-                eget massa.
-			</p>
+			<div className="about-Books">
+				<p>
+					Nam tincidunt, sapien nec congue porta, tellus risus ullamcorper mi, a rutrum justo eros pretium libero. Nullam vel enim id mauris eleifend finibus et ac orci. Aliquam metus massa,
+					aliquam quis arcu sit amet, consectetur faucibus urna. Suspendisse massa diam, efficitur eu massa euismod, pretium lacinia magna. Donec mi orci, sollicitudin in luctus a, varius
+					eget massa.
+				</p>
+			</div>
 		</div>
 		<Container className="container-articles">
 			<Row>
