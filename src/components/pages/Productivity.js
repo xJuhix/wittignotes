5bc8d938dec889d2ffa25e4f834e15bfcb6d534c
xJuhix <49311688+xJuhix@users.noreply.github.com>
2021-01-31 18:57:55 +0100

    Updating styles

--- src/components/pages/Productivity.js
+++ src/components/pages/Productivity.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
 import Heading from '../layout/Heading';
 import ProductivityArticles from '../article/ProductivityArticles';
 import Footer from '../layout/Footer';
@@ -10,30 +11,32 @@ export function Productivity() {
   return (
     <>
       <div className="category">
-        <Row>
-          <Col sm={12} lg={6} className="category__content">
-            <Heading title="Productivity" />
-            <blockquote className="category__blockquote blockquote ">
-              <q className="mb-0">Focus on being productive instead of busy.</q>
-              <footer className="blockquote-footer">Tim Ferris</footer>
-            </blockquote>
-            <div className="category__text">
-              <p>
-                We all are very busy especially in the 21st century.
-                With all these distractions around us staying productive
-                can be incredible hard. There is always another email another
-                text or another meeting. In the rush of the moment we tend to
-                forget that being busy and being productive are two completely
-                different things. In this category I will share some of the
-                productivity hacks that I use and how you can implement them
-                in your life as well.
-              </p>
-            </div>
-          </Col>
-          <Col sm={12} lg={6} className="category__image">
-            <img src={ProductivityImg} className="cateogry-image" alt="Computer and plant" />
-          </Col>
-        </Row>
+        <Container>
+          <Row>
+            <Col sm={12} lg={6} className="category__content">
+              <Heading title="Productivity" />
+              <blockquote className="category__blockquote blockquote ">
+                <q className="mb-0">Focus on being productive instead of busy.</q>
+                <footer className="blockquote-footer">Tim Ferris</footer>
+              </blockquote>
+              <div className="category__text">
+                <p>
+                  We all are very busy especially in the 21st century.
+                  With all these distractions around us staying productive
+                  can be incredible hard. There is always another email another
+                  text or another meeting. In the rush of the moment we tend to
+                  forget that being busy and being productive are two completely
+                  different things. In this category I will share some of the
+                  productivity hacks that I use and how you can implement them
+                  in your life as well.
+                </p>
+              </div>
+            </Col>
+            <Col sm={12} lg={6} className="category__image">
+              <img src={ProductivityImg} className="cateogry-image" alt="Computer and plant" />
+            </Col>
+          </Row>
+        </Container>
       </div>
 
       <h2>Productivity Articles</h2>
